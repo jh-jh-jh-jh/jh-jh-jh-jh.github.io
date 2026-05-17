6a4698aab93d4f4f958733ee5345d5c719e49ea1
jh-jh-jh-jh <yipjh@protonmail.com>
2026-05-18 01:37:00 +0800

    Moved large template string into separate html file
    - moved large html template string from script to html file
    
    - set body box-sizing to border-box to fit screen on mobile
    
    - changed emphasis color to white

---
+++ scripts/load_survivor.js
@@ -0,0 +1,10 @@
+let details = document.getElementById('survivor-details');
+let content = document.getElementById('survivor-content');
+details.addEventListener('toggle', async (event) => {
+    if (event.target.open && !content.innerHTML) {
+        const response = await fetch("./survivor/content.html");
+        const html = await response.text();
+        content.innerHTML = html;
+    }
+});
+
