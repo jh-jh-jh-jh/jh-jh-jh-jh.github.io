f0e3bca308a28915c6867298332ac6d9a6535203
jh-jh-jh-jh <yipjh@protonmail.com>
2026-05-26 21:47:49 +0800

    Redesign

--- scripts/typeout.js
+++ scripts/typeout.js
@@ -7,26 +7,16 @@ let nameString = nameElement.textContent;
 let openBrace = document.getElementById("open-brace");
 let accentColor = getComputedStyle(document.documentElement).getPropertyValue('--accent');
 let headerColor = getComputedStyle(document.documentElement).getPropertyValue('--header-color');
-openBrace.textContent = "";
-openBrace.style.color = headerColor;
-let closeBrace = document.getElementById("close-brace");
-closeBrace.textContent = "";
-closeBrace.style.color = headerColor;
 nameElement.textContent = "";
 caret.style.color = accentColor;
-caret.textContent = "|";
+caret.textContent = "︳";
 (async () => {
     await sleep(500);
     let i = 0;
-    openBrace.textContent = "[";
     await sleep(50);
     while (i < nameString.length) {
         nameElement.textContent += nameString[i++];
         await sleep(50);
     }
-    closeBrace.textContent = "]";
-    closeBrace.style.color = accentColor;
-    openBrace.style.color = accentColor;
-    await sleep(300);
-    caret.hidden = true;
+    setInterval(() => {caret.hidden = !caret.hidden}, 750)
 })();
